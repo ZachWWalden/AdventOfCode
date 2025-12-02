32063c9f747b320c40262576d777856c60608b43
Zachary Walden <zachary.walden@eagles.oc.edu>
2025-12-02 13:35:36 -0600

    day: Dec 2nd finished. Brute Force to ez.

---
+++ 2025/Dec2/mainp2.cpp
@@ -0,0 +1,140 @@
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+struct Range
+{
+	uint64_t start, end;
+};
+
+std::vector<std::string>* splitString(std::string string, char delimeter);
+Range parseRange(std::string range_str);
+uint64_t validateRange(Range range);
+
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		std::cout << "Crap" << std::endl;
+		return -1;
+	}
+
+	std::string filename = argv[1];
+
+	std::fstream file;
+	file.open(filename, std::ios::in);
+
+	std::string line;
+
+	uint64_t sum = 0;
+
+	std::vector<int64_t> left, right;
+	std::vector<std::string>* unparsed_ranges;
+
+	while(std::getline(file, line))
+	{
+		unparsed_ranges = splitString(line, ',');
+	}
+
+	for(std::string rng : (*unparsed_ranges))
+	{
+		sum += validateRange(parseRange(rng));
+	}
+
+	std::cout << "Invalid Sum = " << sum << std::endl;
+
+	return 0;
+}
+
+std::vector<std::string>* splitString(std::string string, char delimeter)
+{
+	std::vector<std::string>* split = new std::vector<std::string>;
+
+	std::size_t pos = 0;
+	std::string splitVal;
+
+	while (pos != std::string::npos)
+	{
+		//std::cout << string << std::endl;
+		pos = string.find(delimeter);
+		//2 sub str, the
+		splitVal = string.substr(0, pos);
+		if(splitVal.length() > 0)
+			split->push_back(splitVal); //push_back 1st token separated by "delimeter"
+		string = string.substr(pos + 1);
+	}
+
+	/*
+	std::vector<std::string>::iterator j;
+	for(j = split->begin(); j < split->end();j++)
+	{
+		std::cout << (*j) << std::endl;
+	}
+	*/
+	return split;
+}
+
+Range parseRange(std::string range_str)
+{
+	Range rng;
+	//split string on '-'
+	std::vector<std::string>* values = splitString(range_str, '-');
+	//convert start and end values to uint64_t.
+	rng.start = std::stoull((*values)[0], nullptr, 10);
+	rng.end = std::stoull((*values)[1], nullptr, 10);
+	return rng;
+}
+
+uint64_t validateRange(Range range)
+{
+	uint64_t sum = 0;
+	//loop through the range
+	for(uint64_t i = range.start; i <= range.end; i++)
+	{
+		//convert to string
+		std::string num = std::to_string(i);
+		//brute force ts.
+		//One id can only be invalid once. pattern must repeat at least twice rpt_len > str_len / 2 -> in valid.
+		//str len
+		//rpt len
+		bool invalid = false;
+		//ensure rpt len divides str len.
+		for(int rpt_len = 1; rpt_len <= num.length() >> 1 && !invalid; rpt_len++)
+		{
+			if((num.length() % rpt_len) == 0)
+			{
+				//check for eqivalency.
+				//calculate num rpts.
+				int num_rpts = num.length() / rpt_len;
+				bool valid = false;
+				//loop through each char in the rpt
+				for(int j = 0; j < rpt_len && !valid; j++)
+				{
+					//compare positional char in each rpt.
+					int offset = rpt_len;
+					for(int k = 1; k < num_rpts && !valid; k++)
+					{
+						if(num[j] != num[j + offset])
+						{
+							//valid id, break
+							valid = true;
+						}
+						offset += rpt_len;
+					}
+				}
+				if(!valid)
+				{
+					invalid = true;
+					sum += i;
+					// std::cout << "Invalid = " << num << std::endl;
+				}
+			}
+		}
+	}
+
+	return sum;
+}
